6b22f6ced118dbaab8969cdc92fa1a3f85bd919f
Shin <53047744+YesYourShin@users.noreply.github.com>
2021-03-29 15:50:52 +0900

    Generic
    Generic

---
+++ OOP/src/Box.java
@@ -0,0 +1,13 @@
+
+public class Box {
+	private String content;
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
---
+++ OOP/src/Box3.java
@@ -0,0 +1,12 @@
+
+public class Box3 {
+	private Student content;
+	
+	public Student getContent() {
+		return content;
+	}
+	
+	public void setContent(Student content) {
+		this.content = content;
+	}
+}
